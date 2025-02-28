2647bf22679498533748008e20edbd6196298f0c
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-02-28 11:25:33 +0700

    chore: xoá command

--- packages/metanode-utils/src/__tests__/formats.test.ts
+++ packages/metanode-utils/src/__tests__/formats.test.ts
@@ -2,7 +2,6 @@ import { formatAddress } from '../formats'
 
 describe('Format Functions', () => {
   test('should format addresses', () => {
-    // format đúng định dạng thành chử thường
     expect(formatAddress('0x295ebd481ad46f96ccae4bfa0ed5c6d74c321032')).toBe(
       '0x295ebd481ad46f96ccae4bfa0ed5c6d74c321032'
     )
--- packages/metanode-utils/src/__tests__/validators.test.ts
+++ packages/metanode-utils/src/__tests__/validators.test.ts
@@ -2,21 +2,15 @@ import { isValidAddress } from '../validators'
 
 describe('Validator Functions', () => {
   test('should validate addresses', () => {
-    // Kiểm tra địa chỉ hợp lệ.
     expect(isValidAddress('0x67f7436714097b96f27e8eae442a889e1de0d131')).toBe(true)
-    // Kiểm tra địa chỉ hợp lệ.
     expect(isValidAddress('0x52908400098527886E0F7030069857D2E4169EE7')).toBe(true)
-    // Kiểm tra địa chỉ hợp lệ.
     expect(isValidAddress('0xde709f2102306220921060314715629080e2fb77')).toBe(true)
     // Chứa ký tự ngoài phạm vi a-f, A-F, 0-9
     expect(isValidAddress('0xINVALIDADDRESS1234567890')).toBe(false)
     // Chỉ có 39 ký tự (thiếu 1)
     expect(isValidAddress('0x742d35Cc6634C0532925a3b844Bc454e4438f44')).toBe(false)
-    // Thiếu "0x" ở đầu
     expect(isValidAddress('742d35Cc6634C0532925a3b844Bc454e4438f44e')).toBe(false)
-    // Chuỗi rỗng
     expect(isValidAddress('')).toBe(false)
-    // Quá ngắn (chỉ có 3 ký tự sau 0x)
     expect(isValidAddress('0x123')).toBe(false)
   })
 })
