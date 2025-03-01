ceef59820ee7b2bc37f0df773c09665da7b28253
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-03-01 10:34:51 +0700

    feat: setup cli

--- packages/metanode-cli/src/commands/create.ts
+++ packages/metanode-cli/src/commands/create.ts
@@ -8,30 +8,44 @@ export default (program: Command) => {
     .command('create')
     .description('Create a new Metanode project.')
     .action(async () => {
-      const projectType = await select({
-        message: 'Please select the type of project you want to create:',
-        choices: LIST_PROJECT
-      })
-      const projectName = await input({
-        message: 'What is the name of the project?'
-      })
-      const projectEnv = await select({
-        message: 'Choose an additional environment file to include:',
-        choices: LIST_ENVIRONMENT
-      })
-      const projectPort = await input({
-        message: 'Choose a port number for development server (default is 3000):',
-        default: '3000',
-        validate: (input) => {
-          const port = parseInt(input, 10)
-          if (isNaN(port) || port < 1 || port > 65535) {
-            return 'Please enter a valid port number (1-65535).'
+      try {
+        const projectType = await select({
+          message: 'Please select the type of project you want to create:',
+          choices: LIST_PROJECT
+        })
+        let projectName
+        while (true) {
+          projectName = await input({
+            message: 'What is the name of the project?'
+          })
+
+          if (!isValidFolderName(projectName)) {
+            console.error('❌ Tên dự án không hợp lệ! Vui lòng thử lại.')
+          } else {
+            break
           }
-          return true
         }
-      })
-      if (!isValidFolderName(projectName)) throw new Error('Tên dự án không hợp lệ!')
-      const projectDirectory = `../apps/${projectName}/`
-      console.log('projectDirectory: ', projectDirectory)
+        const projectEnv = await select({
+          message: 'Choose an additional environment file to include:',
+          choices: LIST_ENVIRONMENT
+        })
+        let projectPort
+        while (true) {
+          projectPort = await input({
+            message: 'Choose a port number for development server (default is 3000):',
+            default: '3000'
+          })
+
+          const port = parseInt(projectPort, 10)
+          if (isNaN(port) || port < 1 || port > 65535) {
+            console.error('❌ Please enter a valid port number (1-65535).')
+          } else {
+            break
+          }
+        }
+      } catch (error) {
+        console.error('⚠️ Đã xảy ra lỗi:', error)
+        process.exit(1)
+      }
     })
 }
