e62f8e2749b5d534772aa81aa943e2768e2f3c7e
PhuSon2809 <phuson2809@gmail.com>
2025-03-04 11:04:55 +0700

    feat: update test case

--- packages/metanode-utils/src/__tests__/handle.test.ts
+++ packages/metanode-utils/src/__tests__/handle.test.ts
@@ -4,6 +4,7 @@ import {
   handleMessageError,
   hexToString,
   hexToUtf8,
+  mulTenPow,
   parseData,
   stringToBytes32
 } from '../handle'
@@ -255,6 +256,36 @@ describe('Handle Functions', () => {
     test('Số 0 với exponent', () => {
       expect(convertExponentialToDecimal(0e5)).toBe('0')
       expect(convertExponentialToDecimal('-0e3')).toBe('0')
+    })
+  })
+
+  describe('mulTenPow', () => {
+    test('Nhân với số mũ dương', () => {
+      expect(mulTenPow(123.45, 2)).toBe('12345')
+      expect(mulTenPow('0.0123', 3)).toBe('12.3')
+      expect(mulTenPow('100', 2)).toBe('10000')
+      expect(mulTenPow('1.5', 3)).toBe('1500')
+    })
+
+    test('Nhân với số mũ âm', () => {
+      expect(mulTenPow('100', -2)).toBe('1')
+      expect(mulTenPow(456, -3)).toBe('0.456')
+      expect(mulTenPow('120', -1)).toBe('12')
+      expect(mulTenPow('1234', -3)).toBe('1.234')
+    })
+
+    test('Không thay đổi khi số mũ là 0', () => {
+      expect(mulTenPow('123', 0)).toBe('123')
+      expect(mulTenPow(0.456, 0)).toBe('0.456')
+    })
+
+    test('Xử lý số 0', () => {
+      expect(mulTenPow('0', 5)).toBe('0')
+      expect(mulTenPow('0', -5)).toBe('0')
+    })
+
+    test('Xử lý undefined hoặc giá trị không hợp lệ', () => {
+      expect(mulTenPow(undefined, 3)).toBe('')
     })
   })
 })
