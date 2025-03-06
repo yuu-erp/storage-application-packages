ae6d90105d93d64d57e6bbbdcf30500fb0f3ed87
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-03-06 09:11:36 +0700

    chore: fix add import

---
+++ apps/metanode-apps/test/vite.config.ts
@@ -0,0 +1,30 @@
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0' // Makes the server accessible externally
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src'),
+      core: path.resolve(__dirname, '../../core'),
+      '@metanode/utils': path.resolve(__dirname, '../../..//packages/metanode-utils/src')
+    }
+  },
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
